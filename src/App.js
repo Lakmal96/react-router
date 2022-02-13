9151349e5176447be406818169e807603133b2c5
Tharindu Lakmal <tharindu96221@gmail.com>
2022-02-13 08:04:01 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import {Route, Switch, Link} from 'react-router-dom';
-import './App.css';
-import About from './Components/About';
+import { Route, Switch, Link } from "react-router-dom";
+import "./App.css";
+import About from "./Components/About";
 import Contact from "./Components/Contact";
 import Services from "./Components/Services";
 
@@ -9,16 +9,32 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
-      <Link to='/'>About</Link>
-      <Link to='/contact'>Contact</Link>
-      <Link to='/services'>Services</Link>
+      <Link to="/">About</Link>
+      <Link to="/contact">Contact</Link>
+      <Link to="/services">Service</Link>
       <Switch>
-        <Route exact path="/" component={About}/>  
+        <Route exact path="/" component={About} />
         {/* <Route exact path="/" render={About}/> Didn't work with useHistory */}
-        <Route exact path="/contact" component={() => <Contact name="Contacts!"/>}/>
-        <Route exact path="/services" component={() => <Services name="Services!!!"/>}/>
-        <Route exact path="/contact/:namePara/:lnamePara" component={() => <Contact name="Contacts!"/>}/>
-        <Route exact path="/services/:service" component={() => <Services name="Services!!!"/>}/>
+        <Route
+          exact
+          path="/contact"
+          component={() => <Contact name="Contacts!" />}
+        />
+        <Route
+          exact
+          path="/services"
+          component={() => <Services name="Services!!!" />}
+        />
+        <Route
+          exact
+          path="/contact/:namePara/:lnamePara"
+          component={() => <Contact name="Contacts!" />}
+        />
+        <Route
+          exact
+          path="/services/:service"
+          component={() => <Services name="Services!!!" />}
+        />
       </Switch>
     </div>
   );
